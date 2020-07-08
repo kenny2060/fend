b4596f6b98ac28c0e212b20595c02da0542a20eb
kenny2060 <kenny2060@hotmail.com>
2020-07-09 00:25:37 +0100

    Wx App Completed
    FEND Lesson 3 Completed

--- projects/weather-journal-app/README.md
+++ projects/weather-journal-app/README.md
@@ -1,10 +1,21 @@
 # Weather-Journal App Project
 
 ## Overview
-This project requires you to create an asynchronous web app that uses Web API and user data to dynamically update the UI. 
+This project was required to create an asynchronous web app that uses a Web API, and user data, to dynamically update the UI.
+File Structure:
+- [Folder] - node_modules
+  - ...
+- [Folder] - Website
+  - app.js
+  - index.html
+  - style.css
+- package-lock.json
+- ReadMe.md
+- server.js
 
 ## Instructions
-This will require modifying the `server.js` file and the `website/app.js` file. You can see `index.html` for element references, and once you are finished with the project steps, you can use `style.css` to style your application to customized perfection.
+It required modifying the `server.js` file and the `website/app.js` file. Setting up a FETCH request to retrieve data from the Web API. In this case, the weather at the users location. The users Zip Code is used.
 
-## Extras
-If you are interested in testing your code as you go, you can use `tests.js` as a template for writing and running some basic tests for your code.
+### NOTE
+**Only a vaild US Zip Code will work (at the moment...)**
+
--- projects/weather-journal-app/website/app.js
+++ projects/weather-journal-app/website/app.js
@@ -1,5 +1,78 @@
 /* Global Variables */
+const baseURL = "https://api.openweathermap.org/data/2.5/weather?zip=";
+const apiKey = "&appid=ece1ff5b0a130ab0dda7f6c1ddea956d";
+const apiUnits = "&units=metric";
+const zipCode = document.getElementById('zip');
+const generateBtn = document.getElementById('generate');
+const holderContent = document.getElementById('feelings');
 
-// Create a new date instance dynamically with JS
+// New date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+const months = ["JAN", "FEB", "MAR","APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"];
+let newDate = d.getDate() + ' ' + months[d.getMonth()] + ' ' + d.getFullYear()
++ ' @ ' + d.getHours() + ':' + d.getMinutes();
+
+// Click listener
+generateBtn.addEventListener('click', getWxData);
+
+function getWxData() {
+  // Call for API Data
+  getApiRes()
+    .then(data => {
+      console.log('Request succeeded with JSON response: ', data)
+      postData('/base', {temp: data.main.temp, userContent: holderContent.value, date: newDate})
+      .catch(error => {
+        console.log('API Request failed', error)
+      })
+      .then(updateUI())
+    });
+
+  // Function get API weather for User Location 
+  // (TODO: User options for setting 'apiUnits')
+  async function getApiRes() {
+    const apiRes = await fetch (baseURL + zipCode.value + apiUnits + apiKey);
+    try {
+      const data = await apiRes.json();
+      return data;
+    }catch (error) {
+      console.log('Error with GET request: ', error);
+    }
+  };
+
+  // Post Data
+  async function postData(url = '', data = {}) {
+    const res = await fetch(url, {
+      method: 'POST',
+      mode: 'cors',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    })
+      .then(res => {
+        // Check header status code
+        if (res.status >= 200 && res.status < 300) {
+          return res.json();
+        } else {
+          throw new Error('Network response not OK: ', res.statusText);
+        }
+      })
+      .catch((error) => console.error('postData FETCH failed', error)
+    );
+  };
+
+  // Update the UI with the Users Content
+  async function updateUI() {
+    const req = await fetch ('/all');
+    try {
+      const allData = await req.json();
+        for (const property in allData) {
+          document.getElementById('temp').innerHTML = Math.round(allData[property].temp) + ' °C';
+          document.getElementById('date').innerHTML = allData[property].date;
+          document.getElementById('content').innerHTML = allData[property].userContent;
+        }
+    } catch (error) {
+      console.log("Error updating UI: ", error);
+    }
+  };
+};
